716121430f609a5dffe6fda078b795cd4e957d56
Shams Ahmed <shamsahmed@me.com>
2015-10-18 20:10:47 +0100

    testing

--- Example/SAStickyHeaderExample/SAStickyHeaderExampleUITests/SAStickyHeaderExampleUITests.swift
+++ Example/SAStickyHeaderExample/SAStickyHeaderExampleUITests/SAStickyHeaderExampleUITests.swift
@@ -9,7 +9,10 @@
 import XCTest
 
 class SAStickyHeaderExampleUITests: XCTestCase {
-        
+    
+    // MARK:
+    // MARK: Setup
+    
     override func setUp() {
         super.setUp()
         
@@ -28,9 +31,11 @@ class SAStickyHeaderExampleUITests: XCTestCase {
         super.tearDown()
     }
     
+    // MARK:
+    // MARK: Test
+    
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
 }
