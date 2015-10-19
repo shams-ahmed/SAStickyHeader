e7c7d1fe39c73232605fc0be17fcd46a5b23572b
Shams Ahmed <shamsahmed@me.com>
2015-10-19 01:04:37 +0100

    use Solid were possible

---
+++ Pod/Classes/SAStickyHeaderView+Observe.swift
@@ -0,0 +1,21 @@
+//
+//  SAStickyHeaderView+Observe.swift
+//  SAStickyHeaderExample
+//
+//  Created by shams ahmed on 19/10/2015.
+//  Copyright © 2015 SA. All rights reserved.
+//
+
+import UIKit
+
+extension SAStickyHeaderView {
+    
+    // MARK:
+    // MARK: KVO
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if let tableView = object as? UITableView {
+            updateImageViewFromScrollEvent(tableView)
+        }
+    }
+}
