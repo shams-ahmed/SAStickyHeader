5603453e7b7f0fa452ece1f0fa3d55c74d378143
Shams Ahmed <shamsahmed@me.com>
2015-11-08 00:36:27 +0000

    fixed example porject

--- Example/SAStickyHeaderExample/ViewController.swift
+++ Example/SAStickyHeaderExample/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 /**
-Github Example images from: https://octodex.github.com/
+Github example images from: https://octodex.github.com
 */
 enum SASGithubImage: String {
     case Gracehoppertocat
@@ -27,22 +27,25 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // list of github images for example, for DEMO purposes ive loadded everything first but this can easiler be added afterworld with images.append()
-        let images = [UIImage(named: SASGithubImage.Gracehoppertocat.rawValue), UIImage(named: SASGithubImage.HipsterPartycat.rawValue), UIImage(named: SASGithubImage.Mountietocat.rawValue), UIImage(named: SASGithubImage.Octoliberty.rawValue), UIImage(named: SASGithubImage.ProfessortocatV2.rawValue)]
-        
-        // add SAStickyHeaderView with a frame and optional image array. 
-        tableView.tableHeaderView = SAStickyHeaderView(frame: CGRectMake(0, 0, CGRectGetWidth(view.frame), 400), table: tableView, image: images)
+        setupView()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
     }
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    // MARK:
+    // MARK: Setup
+    
+    func setupView() {
+        // list of github images for example, for DEMO purposes i've loaded everything first but can easily be added afterworld with images.append()
+        let images = [UIImage(named: SASGithubImage.Gracehoppertocat.rawValue), UIImage(named: SASGithubImage.HipsterPartycat.rawValue), UIImage(named: SASGithubImage.Mountietocat.rawValue), UIImage(named: SASGithubImage.Octoliberty.rawValue), UIImage(named: SASGithubImage.ProfessortocatV2.rawValue)]
+        
+        // add SAStickyHeaderView with a frame and optional image array.
+        tableView.tableHeaderView = SAStickyHeaderView(frame: CGRectMake(0, 0, CGRectGetWidth(view.frame), 400), table: tableView, image: images)
     }
 }
